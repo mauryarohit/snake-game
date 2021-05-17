139043108f929e13ab9d661f68a2ca459984cb57
Rohit Maurya <maurya.rohit704@gmail.com>
2021-05-17 12:20:09 +0530

    snake functionality complete

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -14,17 +14,18 @@ export class BoardComponent implements OnInit {
 
   board = [];
 
-  snakeHead = new Snake(randomNumber(0, BOARD_SIZE), randomNumber(0, BOARD_SIZE));
+  snakeHead = new Snake(randomNumber(0, BOARD_SIZE), randomNumber(0, BOARD_SIZE), Direction.RIGHT);
   snakeTail = this.snakeHead;
 
   running = false;
   gameInterval;
-  currDir = Direction.RIGHT;
+  currDir: Direction = Direction.RIGHT;
   keypress: string;
   dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
   addDir = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
   score = 0;
+  isNewGame = false;
 
   food: {row: number, col: number} = {
     row: randomNumber(0, BOARD_SIZE),
@@ -38,9 +39,29 @@ export class BoardComponent implements OnInit {
 
   }
 
-  // TODO: Logic to end game
-  isGameEnd(): boolean {
-    return true;
+  newGame() {
+    this.snakeHead = new Snake(randomNumber(0, BOARD_SIZE), randomNumber(0, BOARD_SIZE), Direction.RIGHT);
+    this.snakeTail = this.snakeHead;
+    this.currDir = Direction.RIGHT;
+    this.randomFoodOnBoard();
+  }
+
+  endGame() {
+    // this.pauseGame();
+    console.log('Game Over!');
+    this.running = false;
+    this.isNewGame = true;
+    clearInterval(this.gameInterval);
+  }
+
+  checkCollisionWithItself() {
+    let curr = this.snakeHead.next;
+    while(curr != null) {
+      if(curr.row === this.snakeHead.row && curr.col === this.snakeHead.col) {
+        this.endGame();
+      }
+      curr = curr.next;
+    }
   }
 
   getSnake(row: number, col: number): boolean {
@@ -58,7 +79,10 @@ export class BoardComponent implements OnInit {
   @HostListener('window:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
     this.keypress = event.code;
-    if (event.code === 'Space' && !this.running) {
+    if (event.code === 'Space' && this.isNewGame) {
+      this.isNewGame = false;
+      this.newGame();
+    } else if (event.code === 'Space' && !this.running) {
       this.startGame();
     } else if (event.code === 'Space' && this.running) {
       this.pauseGame();
@@ -90,6 +114,7 @@ export class BoardComponent implements OnInit {
       this.preventBackwardMovement();
       this.moveSnake();
       this.checkCollisionWithFood();
+      this.checkCollisionWithItself();
     }, SNAKE_SPEED);
   }
 
@@ -99,25 +124,40 @@ export class BoardComponent implements OnInit {
     clearInterval(this.gameInterval);
   }
 
+  randomFoodOnBoard() {
+    let curr = this.snakeHead;
+    this.food = {
+      row: randomNumber(0, BOARD_SIZE),
+      col: randomNumber(0, BOARD_SIZE)
+    };
+    while(curr != null) {
+      if(this.food.row === curr.row && this.food.col === curr.col) {
+        console.log('snake collision getting new food', this.food.row, this.food.col);
+        this.food = {
+          row: randomNumber(0, BOARD_SIZE),
+          col: randomNumber(0, BOARD_SIZE)
+        };
+        curr = this.snakeHead;
+      }
+      curr = curr.next;
+    }
+  }
+
   checkCollisionWithFood(): void {
     if (this.snakeHead.row === this.food.row && this.snakeHead.col === this.food.col) {
       this.increaseSnakeLengthByOne(this.food.row, this.food.col);
-      this.food = {
-        row: randomNumber(0, BOARD_SIZE),
-        col: randomNumber(0, BOARD_SIZE)
-      };
+      this.randomFoodOnBoard();
     }
   }
 
   increaseSnakeLengthByOne(row: number, col: number): void {
     this.score++;
-    console.log(this.score, row, col);
-    const tmp = new Snake(this.snakeTail.row + this.addDir[this.currDir][0], this.snakeTail.col + this.addDir[this.currDir][1]);
+    const tmp = new Snake(this.snakeTail.row + this.addDir[this.snakeTail.direction][0],
+      this.snakeTail.col + this.addDir[this.snakeTail.direction][1], this.snakeTail.direction);
     if (tmp.row < 0) { tmp.row =  BOARD_SIZE - 1; }
     if (tmp.row >= BOARD_SIZE) { tmp.row =  0; }
     if (tmp.col < 0) { tmp.col =  BOARD_SIZE - 1; }
     if (tmp.col >= BOARD_SIZE) { tmp.col =  0; }
-    console.log(tmp.row, tmp.col);
     this.snakeTail.next = tmp;
     this.snakeTail = this.snakeTail.next;
   }
@@ -126,19 +166,23 @@ export class BoardComponent implements OnInit {
     // Invalid Keypress
     if (this.currDir === Direction.INVALID) { return; }
     let len = 0;
+    this.snakeHead.direction = this.currDir;
     let curr = this.snakeHead;
-    while (curr != null) {
-      len++;
-      // console.log(len, '->', curr.row, curr.col);
-      curr.row = curr.row + this.dir[this.currDir][0];
-      curr.col = curr.col + this.dir[this.currDir][1];
+    let dir: Direction = this.snakeHead.direction;
+    while(curr != null) {
+      curr.row = curr.row + this.dir[curr.direction][0];
+      curr.col = curr.col + this.dir[curr.direction][1];
       if (curr.row < 0) { curr.row =  BOARD_SIZE - 1; }
       if (curr.row >= BOARD_SIZE) { curr.row =  0; }
       if (curr.col < 0) { curr.col =  BOARD_SIZE - 1; }
       if (curr.col >= BOARD_SIZE) { curr.col =  0; }
-      // console.log(curr.row, curr.col);
+
+      const tmp = curr.direction;
+      curr.direction = dir;
+      dir = tmp;
       curr = curr.next;
     }
+
   }
 
   ngOnInit(): void {
--- src/app/models/Snake.ts
+++ src/app/models/Snake.ts
@@ -1,12 +1,16 @@
+import { Direction } from "./Direction";
+
 export class Snake {
 
     row: number;
     col: number;
+    direction: Direction;
     next: Snake;
 
-    constructor(row: number, col: number) {
+    constructor(row: number, col: number, direction: Direction) {
         this.row = row;
         this.col = col;
+        this.direction = direction;
         this.next = null;
     }
 }
